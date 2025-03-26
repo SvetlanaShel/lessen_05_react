3c0de2e7420300aa83003fb87af321b462871b5d
SvetlanaShel <sveta61shel169@gmail.com>
2025-03-26 14:46:41 +0100

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+//Lessons imports
+// импорт компонента Lesson06 по умолчанию
+// при импорте по умолчанию можно использовать другое названия для компонента
+// import Task6 from './lessons/Lesson06/Lesson06';
+// import Lesson06 from './lessons/Lesson06/Lesson06';
+import Lesson07 from "./lessons/Lesson07/Lesson07"
+
+//Homeworks imports
+// import Homework06 from "./homeworks/Homework06/Homework06"
 
 function App() {
 
   return (
     <>
-      First React+TS project
+      {/* <Task6 /> */}
+      {/* <Lesson06 /> */}
+      {/* <Homework06 /> */}
+      <Lesson07 />
     </>
   )
 }
