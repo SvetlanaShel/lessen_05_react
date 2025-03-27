bd176c5fdadf6fab8e388637e339524ae4c28da2
SvetlanaShel <sveta61shel169@gmail.com>
2025-03-27 20:06:58 +0100

    comment

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@
 // при импорте по умолчанию можно использовать другое названия для компонента
 // import Task6 from './lessons/Lesson06/Lesson06';
 // import Lesson06 from './lessons/Lesson06/Lesson06';
+// import Lesson07 from "./lessons/Lesson07/Lesson07";
+import Lesson08 from "./lessons/Lesson08/Lesson08"
 
-import Lesson07 from "./lessons/Lesson06/Lesson07/Lesson07"
 
 //Homeworks imports
 // import Homework06 from "./homeworks/Homework06/Homework06"
+// import Homework07 from "./homeworks/Homework07/Homework07"
+
+//Consultations imports
+import Consultation04 from "./consultations/Consultation04/Consultation04"
 
 function App() {
 
@@ -16,7 +21,10 @@ function App() {
       {/* <Task6 /> */}
       {/* <Lesson06 /> */}
       {/* <Homework06 /> */}
-      <Lesson07 />
+      {/* <Lesson07 /> */}
+      {/* <Homework07/> */}
+      {/* <Lesson08/> */}
+      <Consultation04 />
     </>
   )
 }
