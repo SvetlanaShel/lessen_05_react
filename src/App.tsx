a2c4aa8ca26b4a1d4ec7dc72a1a652cf50ad8321
SvetlanaShel <sveta61shel169@gmail.com>
2025-04-08 19:22:31 +0200

    add commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
+ import  Layout from "./components/Layout/Layout";
+ import { BrowserRouter, Routes, Route }  from 'react-router-dom';
+ import Home from "./pages/Home/Home"
+ import About from "./pages/About/About"
+
  import GlobalStyles from "./styles/GlobalStyles";
 
 //Lessons imports
@@ -23,36 +28,29 @@
  //import Homework11 from "./homeworks/Homework11/Homework11";
  //import Lesson12 from "./lessons/Lesson12/Lesson12";
  //import Homework12 from "./homeworks/Homework12/Homework12";
- import Lesson13 from "./lessons/Lesson13/Lesson13";
+ //import Lesson13 from "./lessons/Lesson13/Lesson13";
  //import Lesson14 from "./lessons/Lesson14/Lesson14";
  //import Homework14 from "./homeworks/Homework14/Homework14";
 
 function App() {
 
   return (
-    <>
+    //BrowserRouter - глобальная обёртка для всего приложения,
+    // которая позволяет использовать маршрутизацию
+    <BrowserRouter>
       <GlobalStyles />
-      {/* <Task6 /> */}
-      {/* <Lesson06 /> */}
-      {/* <Homework06 /> */}
-      {/* <Lesson07 /> */}
-      {/* <Homework07/> */}
-      {/* <Lesson08/> */}
-      {/*<Consultation04 />*/}
-      {/*<Homework08/>*/}
-       {/*<Lesson09 />*/}
-       {/*<Homework09 />*/}
-       {/*<Lesson10Container/>*/}
-       {/*<Homework10/>*/}
-       {/*<Lesson11 />*/}
-       {/*<Homework11 />*/}
-       {/*<Lesson12 />*/}
-       {/*<Homework12 />*/}
-       <Lesson13 />
-       {/*<Lesson14 />*/}
        {/*<Homework14 />*/}
-    </>
+       <Layout>
+       {/* Routes - обертка, которая собирает все маршруты приложения */}
+       <Routes>
+          {/* Route - компонент библиотеки, в который передаётся маршрут и тот компонент (страницу),
+          который должен быть вызван, если выполнен переход по этому маршруту */}
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+        </Routes>
+      </Layout>
+    </BrowserRouter>
   )
-};
+}
 
 export default App;
---
+++ src/components/Layout/types.ts
@@ -0,0 +1,4 @@
+import { ReactNode } from "react";
+export interface LayoutProp {
+ children: ReactNode
+}
